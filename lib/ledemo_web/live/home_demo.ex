fd676f346edf3a7f4f4936effa3707f66972cc4a
andyl <andy@r210.com>
2020-01-21 12:58:44 -0800

    Misc updates

--- lib/ledemo_web/live/home_demo.ex
+++ lib/ledemo_web/live/home_demo.ex
@@ -1,7 +1,7 @@
 defmodule LedemoWeb.Live.HomeDemo do
   use Phoenix.LiveView
 
-  import Phoenix.LiveEditable
+  use Phoenix.LiveEditable
 
   def mount(_session, socket) do
     {:ok, socket}
@@ -9,15 +9,25 @@ defmodule LedemoWeb.Live.HomeDemo do
 
   def render(assigns) do
     ~L"""
+    <style>
+      .editable-click { cursor: pointer;}
+    </style>
+
     <b style='margin-right: 60px;'>TRY IT!</b>
     <span style='margin-right: 60px;'>
-    Edit This Field: <%= live_edit assigns, "TEXT", id: "home_demo", type: "text", on_submit: "update" %>
+    Edit This Field: <%= live_edit(assigns, "TEXT", id: "home_demo", type: "text", on_submit: "update") %>
     </span>
     Value: BONGGG
+    <p><p/>
+
+    <%= live_edit(assigns, "asdf", type: "text", id: "one",   on_submit: 'saveit') %><br/>
+    <%= live_edit(assigns, "qwer", type: "text", id: "two",   on_submit: 'saveit') %><br/>
+    <%= live_edit(assigns, "zxcv", type: "text", id: "three", on_submit: 'saveit') %><br/>
     """
   end
 
-  def handle_event("update", value, socket) do
+  def handle_event(tag, value, socket) do
+    IO.inspect tag
     IO.inspect value
     IO.inspect socket
     {:ok, socket}
