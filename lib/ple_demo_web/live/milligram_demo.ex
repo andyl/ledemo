311982ebb1f65fb7ff97e31d10ad71e20b326f08
andyl <andy@r210.com>
2020-01-25 09:03:37 -0800

    Add milligram page

---
+++ lib/ple_demo_web/live/milligram_demo.ex
@@ -0,0 +1,45 @@
+defmodule PleDemoWeb.Live.MilligramDemo do
+  use Phoenix.LiveView
+
+  use Phoenix.LiveEditable
+
+  def mount(_session, socket) do
+    Phoenix.LiveEditable.set_framework(Phoenix.LiveEditable.Milligram)
+    opts = %{
+      test_1: "1 - CLICK TO EDIT",
+      test_2: "2 - CLICK TO EDIT"
+    }
+    {:ok, assign(socket, opts)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <div class="ple_wrapper">
+
+    <div class='header_cell'><b>Field Type</b></div>
+    <div class='header_cell'><b>Editable Fields</b></div>
+
+    <div>Text Field - Inline</div>
+    <div>
+    <%= live_edit(assigns, @test_1, type: "text", id: "test_1", on_submit: "update") %>
+    </div>
+
+    <div>Text Field - Inline</div>
+    <div>
+    <%= live_edit(assigns, @test_2, type: "text", id: "test_2", on_submit: "update") %>
+    </div>
+
+    </div>
+    """
+  end
+
+  def handle_event("update", %{"editable_text" => new_val}, socket) do
+    case socket.assigns.focus do
+      nil -> 
+        {:noreply, socket}
+      focus -> 
+        key = focus |> String.to_atom()
+        {:noreply, socket |> assign(key, new_val) |> assign(:focus, nil)}
+    end
+  end
+end
--- lib/ple_demo_web/views/layout_view.ex
+++ lib/ple_demo_web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule PleDemoWeb.LayoutView do
   use PleDemoWeb, :view
+
+  def header_nav(conn) do
+    ph = conn.request_path
+    {:safe, "#{mi(ph, "Home")} | #{mi(ph, "Milligram")} | #{mi(ph, "Bootstrap4")}"}
+  end
+
+  defp mi("/", "Home"), do: "<b>Home</b>"
+  defp mi(_, "Home"), do: "<a href='/'>Home</a>"
+  defp mi("/bootstrap4", "Bootstrap4"), do: "<b>Bootstrap4</b>"
+  defp mi(_, "Bootstrap4"), do: "<a href='/bootstrap4'>Bootstrap4</a>"
+  defp mi("/milligram", "Milligram"), do: "<b>Milligram</b>"
+  defp mi(_, "Milligram"), do: "<a href='/milligram'>Milligram</a>"
 end
